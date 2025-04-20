65c3e38db80b7880e5eb805400d8f5ed78168980
firdavs9777 <fmutalipov97@gmail.com>
2025-04-20 19:58:25 +0900

    modal added to bananatalk

---
+++ src/composables/Modal.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Modal.css";
+import { IoClose } from "react-icons/io5";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null; // Do not render modal if not open
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="close-modal" onClick={onClose}>
+          <IoClose size={24} />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
